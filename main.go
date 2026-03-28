990bafaaa2568a550636f7ad620636ef731eb0cb
luanfv <luanfrancavieira@gmail.com>
2026-03-28 15:06:10 -0300

    feat: get tax rates from json

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
 	"os"
@@ -9,8 +10,35 @@ import (
 )
 
 var currencyMapping = map[string]float64{
-	"USD": 0.151,
-	"EUR": 0.137,
+    "USD": 0.151,
+    "EUR": 0.137,
+    "JPY": 16.29,
+    "GBP": 0.13,
+    "CHF": 0.1402,
+    "AUD": 0.2712,
+    "CAD": 0.2374,
+    "CNY": 1.251,
+    "HKD": 1.326,
+    "NZD": 0.2922,
+    "SEK": 1.655,
+    "NOK": 1.806,
+    "DKK": 1.122,
+    "SGD": 0.2249,
+    "KRW": 242.97,
+    "ZAR": 3.239,
+    "MXN": 3.454,
+    "INR": 14.71,
+    "ILS": 0.63,
+    "THB": 5.74,
+    "IDR": 2875.0,
+    "MYR": 0.754,
+    "PHP": 9.74,
+    "PLN": 0.644,
+    "CZK": 3.77,
+    "HUF": 61.59,
+    "TRY": 6.49,
+    "BGN": 0.293,
+    "RON": 0.746,
 }
 
 type CurrencyConverter struct {
@@ -18,12 +46,30 @@ type CurrencyConverter struct {
 	rate float64
 }
 
+type TaxJson struct {
+	Rates map[string]float64 `json:"rates"`
+}
+
 func (cc *CurrencyConverter) SetValues(money string, currency string) (*CurrencyConverter, error) {
 	moneyValue, err := strconv.ParseFloat(money, 64)
 	if err != nil {
 		return nil, errors.New("Invalid money value.")
 	}
-	rate, ok := currencyMapping[strings.ToUpper(currency)]
+
+	file, err := os.ReadFile("tax2.json")
+	if err != nil {
+		rate, ok := currencyMapping[strings.ToUpper(currency)]
+		if !ok {
+			return nil, errors.New("Unsupported currency code.")
+		}
+		cc.money = moneyValue
+		cc.rate = rate
+		return cc, nil
+	}
+	
+	var result TaxJson
+	json.Unmarshal(file, &result)
+	rate, ok := result.Rates[strings.ToUpper(currency)]
 	if !ok {
 		return nil, errors.New("Unsupported currency code.")
 	}
@@ -57,7 +103,7 @@ func main() {
 	}
 
 	currency := CurrencyConverter{}
-	_, err :=currency.SetValues(os.Args[1], os.Args[2])
+	_, err := currency.SetValues(os.Args[1], os.Args[2])
 	if err != nil {
 		fmt.Printf("Error: %v\n", err)
 		return
